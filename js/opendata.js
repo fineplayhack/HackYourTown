ab99a761544299d4c4a2db403fd96473fd371dd7
takumi <rumantakumi@gmail.com>
2017-12-20 18:22:29 +0900

    とりあえずコミット．バグあるので注意！

---
+++ js/opendata.js
@@ -0,0 +1,27 @@
+var jsondata = "https://raw.githubusercontent.com/wakayama-pref-org/pr_magazine_event_2017_12/master/json/pr_magazine_event_2017_12.json";
+$(function() {
+  $.getJSON(jsondata , function(data) {
+    var ulObj = $("#demo"),
+    len = data.length;
+    console.log("json", data);
+    //console.log("json[0]", data[0]["見出し"]);
+    //console.log("jsonlen", len);
+    for(var i = 0; i < len; i++) {
+      if(data[i]["対象"].indexOf("小学生") != -1)
+        ulObj.append($("<li>").attr({"id":i}).text(data[i]["開催日"]));
+        opdata[] = [
+          title : text(data[i]["見出し"])
+          start : text(data[i]["開催日"])
+        ];
+    }
+    // for(var i = 0; i < len; i++) {
+    //   ulObj.append($("<li>").attr({"id":i}).text(data[i]));
+    // }
+  });
+
+  $('#calendar').fullCalendar({
+    editable : true,
+    events : opdata[],
+  });
+
+});
